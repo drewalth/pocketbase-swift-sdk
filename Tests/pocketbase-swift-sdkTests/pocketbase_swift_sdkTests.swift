38c425ad112935a7c407fa5358983be02b085ae4
drewalth <16584198+drewalth@users.noreply.github.com>
2025-07-12 16:19:07 -0600

    Initial Commit

---
+++ Tests/pocketbase-swift-sdkTests/pocketbase_swift_sdkTests.swift
@@ -0,0 +1,6 @@
+import Testing
+@testable import pocketbase_swift_sdk
+
+@Test func example() async throws {
+    // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+}
